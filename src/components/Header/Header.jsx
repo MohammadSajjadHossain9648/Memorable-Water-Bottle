7f1138daca7ec576f3cee5950ad46e4e0342e7a0
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-10-21 21:24:44 +0600

    Create Header component for website header design

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h2 className='header_title'>Memorable Water Bottle</h2>
+        </div>
+    );
+};
+
+export default Header;
